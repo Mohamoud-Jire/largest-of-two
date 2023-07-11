d8f76d32b28704c3988e0e7e358e38c52b0bda25
Mohamoud-Jire <amoh54932@gmail.com>
2023-07-11 23:43:09 +0300

    Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let x = 10;
+let y = 15;
+if (x > y) {
+  console.log(`The largest number out of the two numbers is ${x}`);
+} else {
+  console.log(`The largest number out of the two is ${y}`);
+}
